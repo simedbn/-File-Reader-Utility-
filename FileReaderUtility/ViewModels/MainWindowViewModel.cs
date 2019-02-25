a54d07c2bfd64e85e89e2c67d99019bb0fa95dad
Bounzaha Mohamed <simedbounzaha@gmail.com>
2019-02-25 22:40:00 +0000

    Version 1.3

--- FileReaderUtility/ViewModels/MainWindowViewModel.cs
+++ FileReaderUtility/ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,12 @@
 using Prism.Commands;
 using Prism.Interactivity.InteractionRequest;
 using Prism.Mvvm;
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
 
 namespace FileReaderUtility.ViewModels
 {
@@ -14,6 +18,10 @@ namespace FileReaderUtility.ViewModels
         private string _title = "File reader utility";
         private string _filePath;
         private string _fileResult;
+        private ObservableCollection<string> _encryptionAlgorithms;
+        private string _selectedEncryptionAlgorithm;
+        private string _selectedFileType;
+        private ObservableCollection<string> _fileTypeList;
         #endregion
 
         #region Properties
@@ -36,13 +44,49 @@ namespace FileReaderUtility.ViewModels
         }
 
         /// <summary>
-        /// File Path.
+        /// File Result.
         /// </summary>
         public string FileResult
         {
             get { return _fileResult; }
             set { SetProperty(ref _fileResult, value); }
         }
+
+        /// <summary>
+        /// Selected Encryption Algorithm
+        /// </summary>
+        public string SelectedEncryptionAlgorithm
+        {
+            get { return _selectedEncryptionAlgorithm; }
+
+            set
+            {
+                SetProperty(ref _selectedEncryptionAlgorithm, value);
+            }
+        }
+
+        /// <summary>
+        /// Selected File Type
+        /// </summary>
+        public string SelectedFileType
+        {
+            get { return _selectedFileType; }
+
+            set
+            {
+                SetProperty(ref _selectedFileType, value);
+            }
+        }
+
+        /// <summary>
+        /// Encryption Algorithm List
+        /// </summary>
+        public ObservableCollection<string> EncryptionAlgorithmList { get => _encryptionAlgorithms; set => SetProperty(ref _encryptionAlgorithms, value); }
+
+        /// <summary>
+        /// Encryption Algorithm List
+        /// </summary>
+        public ObservableCollection<string> FileTypeList { get => _fileTypeList; set => SetProperty(ref _fileTypeList, value); }
 
         /// <summary>
         /// Interaction request to open the file.
@@ -64,12 +108,18 @@ namespace FileReaderUtility.ViewModels
         /// </summary>
         private void OpenFileDialogMethod()
         {
+            if (string.IsNullOrEmpty(SelectedFileType))
+            {
+                MessageBox.Show("Please Select a file type.");
+                return;
+            }
+
             // The open file dialog confirmation.
             var fileConfirmation = new FileDialogConfirmation();
 
             fileConfirmation.Title = "Choose the file to read";
             fileConfirmation.AddExtension = true;
-            fileConfirmation.Filter = "Text files (*.txt)|*.txt|XML (*.*)|*.xml";
+            fileConfirmation.Filter = $"{SelectedFileType} files (*.{SelectedFileType})|*.{SelectedFileType}";
 
             // Show the dialog.
             OpenFileRequest.Raise(fileConfirmation, (of) =>
@@ -77,8 +127,82 @@ namespace FileReaderUtility.ViewModels
                 // Ge the result
                 FilePath = of.FileName;
                 if (FilePath != null)
-                    FileResult = File.ReadAllText(of.FileName);
+                {
+                    string text = File.ReadAllText(of.FileName);
+                    if (text?.Length > 0)
+                    {
+                        switch (SelectedEncryptionAlgorithm)
+                        {
+                            case "AES":
+                                FileResult = EncryptTextByAes(text);
+                                break;
+                            case "RSA":
+                                FileResult = EncryptTextByRsa(text);
+                                break;
+                            default:
+                                FileResult = text;
+                                break;
+                        }
+                    }
+                }
             });
+        }
+
+        /// <summary>
+        /// Encrypt Text By AES Algorithm
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string EncryptTextByAes(string text)
+        {
+            SymmetricAlgorithm aesAlgorithm = new AesManaged();
+            // Create an encryptor from the AES algorithm instance and pass the aes algorithm key and inialiaztion vector to generate a new random sequence each time for the same text  
+            ICryptoTransform encryptor = aesAlgorithm.CreateEncryptor(aesAlgorithm.Key, aesAlgorithm.IV);
+
+            // Create a memory stream to save the encrypted data in it  
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter writer = new StreamWriter(cs))
+                    {
+                        // Write the text in the stream writer   
+                        writer.Write(text);
+                    }
+                }
+
+                // Get the result as a byte array from the memory stream   
+                byte[] encryptedDataBuffer = ms.ToArray();
+
+                return Encoding.UTF8.GetString(encryptedDataBuffer);
+            }
+        }
+
+        /// Encrypt Text By RSA Algorithm
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string EncryptTextByRsa(string text)
+        {
+
+            // Convert the text to an array of bytes   
+            UnicodeEncoding byteConverter = new UnicodeEncoding();
+            byte[] dataToEncrypt = byteConverter.GetBytes(text);
+
+            // Create a byte array to store the encrypted data in it   
+            byte[] encryptedData;
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                string publicKey = rsa.ToXmlString(false); // false to get the public key   
+
+                // Set the rsa pulic key   
+                rsa.FromXmlString(publicKey);
+
+                // Encrypt the data and store it in the encyptedData Array   
+                encryptedData = rsa.Encrypt(dataToEncrypt, false);
+
+                return Encoding.UTF8.GetString(encryptedData);
+            }
         }
         #endregion
 
@@ -87,6 +211,17 @@ namespace FileReaderUtility.ViewModels
         {
             OpenFileRequest = new InteractionRequest<FileDialogConfirmation>();
             OpenFileDialogCommand = new DelegateCommand(OpenFileDialogMethod);
+            EncryptionAlgorithmList = new ObservableCollection<string>
+            {
+                 "RSA",
+                 "AES"
+            };
+            FileTypeList = new ObservableCollection<string>
+            {
+
+                 "TXT",
+                 "XML"
+            };
         }
     }
 }
